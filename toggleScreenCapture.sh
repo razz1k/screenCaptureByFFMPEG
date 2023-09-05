50b8d9d7dcc365e5b0f391d6c92b2cb455293222
razz1k <zdeadmenz@gmail.com>
2023-09-05 21:25:21 +0300

    add noiceReduction and new way to set file name

--- toggleScreenCapture.sh
+++ toggleScreenCapture.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 pidOfFFMPEG=$(ps aux | grep -i 'ffmpeg -f x11grab' | grep -v 'grep' | awk '{print $2}')
-pathToOutput="/home/razz1k/Videos"
+filePathOutput="/home/$USER/Videos"
+filePrefix="screenCapture_"
+fileFullName="$filePathOutput/$filePrefix$(date +'%H-%M-%S').mkv"
 
 if [ -z $pidOfFFMPEG ]; then
+  amixer -c 1 sset Capture 65% > /dev/null
   ffmpeg \
 -f x11grab -i :0.0 \
--f pulse -i default -ac 2 -af "highpass=f=100, lowpass=f=3000, volumedetect, volume=2" \
+-f pulse -i default -ac 2 \
+-af "highpass=f=100, lowpass=f=3000, volumedetect, volume=2.3" \
 -c:a aac -b:a 160k \
--c:v libx264 -preset slow -crf 10 -movflags +faststart -nostdin -loglevel panic \
-$pathToOutput/screenCapture_$(date +'%H-%M-%S').mkv &
+-c:v libx264 -preset slow -crf 10 \
+-movflags +faststart \
+-nostdin -loglevel panic \
+$fileFullName &
 else
-  sleep 1 && kill $pidOfFFMPEG &
+  sleep 0.5 && kill $pidOfFFMPEG &
 fi
